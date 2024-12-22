baaa3595d5f4c4315ad8ac66497977de81f82a29
yannmerakeb <yann.merakeb@gmail.com>
2024-12-22 17:33:33 +0100

    update blapi (commented to avoid bugs) and strategies

--- my_package/data_inputs/__init__.py
+++ my_package/data_inputs/__init__.py
@@ -2,12 +2,19 @@ from .binance_api import BinanceDataInput
 from .custom_input import CustomDataInput
 from .df_input import DataFrameDataInput
 from .yahoo_api import YahooDataInput
-from .bloomberg_api import BLPApi
+#from .bloomberg_api import BLPApi
 
 __all__ = [
     "BinanceDataInput",
     "CustomDataInput",
     "DataFrameDataInput",
     "YahooDataInput",
-    "BLPApi",
 ]
+
+'''__all__ = [
+    "BinanceDataInput",
+    "CustomDataInput",
+    "DataFrameDataInput",
+    "YahooDataInput",
+    "BLPApi",
+]'''
